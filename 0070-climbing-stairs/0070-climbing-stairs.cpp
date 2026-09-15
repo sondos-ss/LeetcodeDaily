d42469e7ecc5a252b24614a788a0b7bfb4e23463
Sondos Said <143421662+sondos-ss@users.noreply.github.com>
2026-09-15 23:40:25 +0300

    Time: 0 ms (100%), Space: 7.9 MB (58.93%) - LeetHub-Neo

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dp = vector<int>(46, -1);
+    int climbStairs(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
